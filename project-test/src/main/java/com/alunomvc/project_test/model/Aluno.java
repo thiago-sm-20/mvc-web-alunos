42ba382dfd40c2ab602f583d579fc048b86bcc13
Thiago Mambrini <mambrini.thiago2005@gmail.com>
2026-05-31 11:18:24 -0300

    Add files via upload

---
+++ project-test/src/main/java/com/alunomvc/project_test/model/Aluno.java
@@ -0,0 +1,24 @@
+package com.alunomvc.project_test.model;
+
+public class Aluno {
+    
+    private String nome;
+    private String matricula;
+
+    public Aluno(String nome, String matricula) {
+        if(nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome obrigatório");
+        }
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+} 
